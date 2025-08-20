e435c7d7244bfea19daa93ef53732c8f0447340f
数字游牧人 <admin@flyweb.cn>
2025-08-20 12:57:07 +0800

    Update Subconverter.vue
    update backendOptions

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -232,7 +232,7 @@ export default {
           Trojan: "trojan",
           Surge3: "surge&ver=3",
         },
-        backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }],
+        backendOptions: [{ value: "https://subconverter.6010.top/sub?" }],
         remoteConfig: [
           {
             label: "universal",
